9f34661dba10fcf58edecfc5a623132fe6588b4b
Amanda <wiltbergeramanda@gmail.com>
2024-06-12 15:16:41 -0500

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById("create-task-form")
+  form.addEventListener("submit", (event) => {
+  event.preventDefault()
+  let text = document.getElementById("new-task-description").value 
+  let task = document.getElementById("tasks")
+  let newElement = document.createElement("li")
+  newElement.textContent = text
+  task.appendChild(newElement)
+})
 });
